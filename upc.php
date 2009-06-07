cdb6cd691d52c7566d8d2796ceb53d145bcd2bc0
hacsat <hacsat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-07 13:49:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/user-post-count/trunk@123534 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ upc.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: User Post count
+Plugin URI:
+Description: Shows the total count of post by author.
+Author: Hacsat
+Version: 1.0
+Author URI: http://babysofts.com
+*/
+
+function upc_wp_dashboard() {
+ include("usercount.php");
+}
+function upc_wp_dashboard_add() {
+	wp_add_dashboard_widget( 'upc_wp_dashboard', __( 'User Post Count' ),'upc_wp_dashboard');
+}
+add_action('wp_dashboard_setup', 'upc_wp_dashboard_add');
+
+?>
